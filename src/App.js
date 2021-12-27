5c89159bc24ad814b825f81e7dab69fbae006a95
Prathap-Karaka <venkatasivaprathap1002@gmail.com>
2021-12-27 08:55:16 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header"></div>
+      <div className="body"></div>
+      <div className="footer"></div>
+    </div>
+  );
+}
+
+export default App;
