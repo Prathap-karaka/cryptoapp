a3185351b322868e8f38dc7dcb110a2b25b27ba0
Prathap-Karaka <venkatasivaprathap1002@gmail.com>
2021-12-28 15:01:44 +0530

    first commit after

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Layout, Typography, Space } from "antd";
 function App() {
   return (
     <div className="App">
-      <div className="navbar"></div>
+      <div className="navbar">hello</div>
       <div className="body"></div>
       <div className="footer"></div>
     </div>
