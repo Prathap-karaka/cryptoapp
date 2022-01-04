4fdc65558f6782e73f3250c0642f91825149f345
Prathap-Karaka <venkatasivaprathap1002@gmail.com>
2022-01-04 12:29:19 +0530

    checking commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  { Button,Avatar, Menu, Typography } from 'antd';
+const NavBar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NavBar
